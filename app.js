12ac7543c53ca6ea83b2e1b83fa737b70c22e55b
BeckHarper13 <beckharper13@gmail.com>
2025-02-06 12:40:49 -0800

    created vercel.json file

--- app.js
+++ app.js
@@ -175,5 +175,7 @@ const server = http.createServer((req, res) => {
 // Start the server
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on port ${process.env.PORT || 3000}`);
+
+
 });
